57695aa920900dc6b0f70100bd8ef8b005cf8fea
andrewPi <andygerth1@gmail.com>
2024-12-12 13:19:40 -0800

    Pi Working at 320x240 on extended display

--- tkinter_test1.py
+++ tkinter_test1.py
@@ -76,9 +76,12 @@ def buttonpushed(numba):
 ## Setup Block for main GUI window (root)
 root = Tk()
 aspect_ratio = 4/3
-main_width = 680
-main_height = int(main_width/aspect_ratio)
-root.geometry("{}x{}".format(main_width, main_height))
+main_width = 320
+#main_height = int(main_width/aspect_ratio)
+main_height = 240
+xoffset = 640+main_width
+root.geometry("{}x{}+{}+0".format(main_width, main_height, xoffset))
+#root.attributes('-fullscreen', True)
 root.title("DnD Roller")
 root.config(bg="skyblue")
 
@@ -94,20 +97,16 @@ right_frame = Frame(root)
 right_frame.columnconfigure(1,weight=1)
 
 ## Pack in said columns of three frames left to right
-left_frame.pack(expand=True,fill='x', side='left', padx=10, pady=10)
-middle_frame.pack(expand=True,fill='x', side='left', padx=10, pady=10)
-right_frame.pack(expand=True,fill='x', side='left', padx=10, pady=10)
+left_frame.pack(expand=True,fill='x', side='left', padx=5, pady=5)
+middle_frame.pack(expand=True,fill='x', side='left', padx=5, pady=5)
+right_frame.pack(expand=True,fill='x', side='left', padx=5, pady=5)
 
 ## Button setup for Left Frame (advantage selection buttons)
 adv_button_width = 12
 adv_button_height = 8
-buttonAdv = Button(left_frame, text="Advantage", command=lambda: advfun("Advantage"),
-                   width=adv_button_width, height=adv_button_height, borderwidth=10)
-buttonDisAdv = Button(left_frame, text="Disadvantage", command=lambda: advfun("Disadvantage"),
-                      width=adv_button_width, height=adv_button_height, borderwidth=10)
-buttonNat = Button(left_frame, text="Natural", command=lambda: advfun("Natural"),
-                   width=adv_button_width, height=adv_button_height, highlightbackground="purple2",
-                   borderwidth=10)
+buttonAdv = Button(left_frame, text="Adv", command=lambda: advfun("Advantage"))
+buttonDisAdv = Button(left_frame, text="Dis", command=lambda: advfun("Disadvantage"))
+buttonNat = Button(left_frame, text="Nat", command=lambda: advfun("Natural"))
 buttonAdv.pack()
 buttonDisAdv.pack()
 buttonNat.pack()
@@ -116,16 +115,11 @@ buttonNat.pack()
 ## Button setup for Middle Frame (dice roll selections)
 roll_button_width = 8
 roll_button_height = 5
-button4 = Button(middle_frame, text="d4", command=lambda: buttonpushed(4),
-                 width=roll_button_width, height=roll_button_height)
-button6 = Button(middle_frame, text="d6", command=lambda: buttonpushed(6),
-                 width=roll_button_width, height=roll_button_height)
-button8 = Button(middle_frame, text="d8", command=lambda: buttonpushed(8),
-                 width=roll_button_width, height=roll_button_height)
-button12 = Button(middle_frame, text="d12", command=lambda: buttonpushed(12),
-                 width=roll_button_width, height=roll_button_height)
-button20 = Button(middle_frame, text="d20", command=lambda: buttonpushed(20),
-                 width=roll_button_width, height=roll_button_height)
+button4 = Button(middle_frame, text="d4", command=lambda: buttonpushed(4))
+button6 = Button(middle_frame, text="d6", command=lambda: buttonpushed(6))
+button8 = Button(middle_frame, text="d8", command=lambda: buttonpushed(8))
+button12 = Button(middle_frame, text="d12", command=lambda: buttonpushed(12))
+button20 = Button(middle_frame, text="d20", command=lambda: buttonpushed(20))
 button4.pack()
 button6.pack()
 button8.pack()
@@ -134,16 +128,16 @@ button20.pack()
 
 
 ## Right Frame setup (grid pack 3x2 text labels and roll result displays)
-result1_label = Label(right_frame, text="Adv/Dis Roll 1")
-result2_label = Label(right_frame, text="Adv/Dis Roll 2")
-result_label = Label(right_frame, text="Final Result")
+result1_label = Label(right_frame, text="Roll 1")
+result2_label = Label(right_frame, text="Roll 2")
+result_label = Label(right_frame, text="Final Roll")
 result1_label.grid(row=0, column=0)
 result2_label.grid(row=1, column=0)
 result_label.grid(row=2, column=0)
 
-outputlabel1 = Label(right_frame, textvariable=currentResult1, width=8, height=3, bg='light sea green')
-outputlabel2 = Label(right_frame, textvariable=currentResult2, width=8, height=3, bg='cadet blue')
-outputlabel = Label(right_frame, textvariable=currentResult, width=8, height=3, bg='light slate blue')
+outputlabel1 = Label(right_frame, textvariable=currentResult1, bg='light sea green')
+outputlabel2 = Label(right_frame, textvariable=currentResult2, bg='cadet blue')
+outputlabel = Label(right_frame, textvariable=currentResult, bg='light slate blue')
 outputlabel1.grid(row=0, column=1, sticky='E')
 outputlabel2.grid(row=1, column=1, sticky='E')
 outputlabel.grid(row=2, column=1, sticky='E')
